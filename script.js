0ef7c1f67d682dbc570d4c0bff682317f20f8e19
MarceIine <ac405040500@gmail.com>
2025-02-17 05:08:59 -0400

    Replace profilePicutureLight with Peter Griffin

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const body = document.body;
 const darkBackgroundVideo = "https://videos.pexels.com/video-files/2895754/2895754-uhd_2732_1440_25fps.mp4";
 const lightBackgroundVideo = "https://videos.pexels.com/video-files/2935032/2935032-uhd_2560_1440_30fps.mp4";
 const darkProfileVideo = "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExdjYzdWd2MDBiMG8wZmUzbDVkYjJheWhwcGNiczJxdDU2dGZldjdleCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/p1sPKlISSvMpa/giphy.mp4";
-const lightProfileVideo = "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZG1qdW9vamE1cGR1MDM4aDhtY2Z2eThkcXIzaDg1ZHcxc3Jzc2lnZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/kZqbBT64ECtjy/giphy.mp4";
+const lightProfileVideo = "https://i.makeagif.com/media/12-12-2015/JoZDIX.mp4";
 
 // Check system preference
 const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
